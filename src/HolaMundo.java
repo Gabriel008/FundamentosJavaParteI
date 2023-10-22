1f45de0be755f3eb19d077258eef1a229a32b631
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-10-22 00:35:24 -0300

    Curso Java HolaMundo Cap.5

---
+++ src/HolaMundo.java
@@ -0,0 +1,17 @@
+//Mi clase JAVA
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author gabri
+ */
+public class HolaMundo {
+    public static void main(String args[]){
+    System.out.println("HelloWord!!!!");
+    }
+    
+    
+}
