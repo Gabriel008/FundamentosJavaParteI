d2e994570f31836913f16b9e0fea36a1ba7f4892
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-01 17:51:41 -0300

    Curso Java - Tipos Numericos Enteros y maximos  Cap.16

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -22,5 +22,21 @@ public class HolaMundo {
         System.out.println("Valor minimo byte: "+Byte.MIN_VALUE); 
         System.out.println("Valor maximo byte: "+Byte.MAX_VALUE); 
         
+        short numeroShort = (short)32768;
+        System.out.println("\nnumeroShort = " + numeroShort);
+        System.out.println("Valor minimo short: "+Short.MIN_VALUE); 
+        System.out.println("Valor maximo short: "+Short.MAX_VALUE); 
+        
+        int numeroInt = (int)2147483648L;
+        System.out.println("\nnumeroInt = " + numeroInt);
+        System.out.println("Valor minimo int: "+Integer.MIN_VALUE); 
+        System.out.println("Valor maximo int: "+Integer.MAX_VALUE); 
+        
+        long numeroLong = (long)9223372036854775808D;
+        System.out.println("\numeroLong = " + numeroLong);
+        System.out.println("Valor minimo long: "+Long.MIN_VALUE); 
+        System.out.println("Valor maximo long: "+Long.MAX_VALUE); 
+        
+        
     }
 }
