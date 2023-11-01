97f277d8e49847224aeba19279ef4c658de9ef47
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-01 00:16:40 -0300

    Curso Java - Caracteres Especiales Cap.12

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -11,17 +11,12 @@
 public class HolaMundo {
 
     public static void main(String args[]) {
-        var usuario = "Juan";
-        var titulo = "Ingeniero";
-        var union = titulo + " " + usuario;
-        System.out.println("union = " + union);
-            
-        var i=3.5;
-        var j=4;
+        var nombre = "Karla";
+        System.out.println("Nueva linea: \n"+ nombre); // Salto de linea -> \n
+        System.out.println("Tabulador: \t"+ nombre); // Tabulador -> \t
+        System.out.println("Retroceso: \b\b"+ nombre); // Retroceso -> \b
+        System.out.println("Comilla Simple: \'"+ nombre +"'"); // Comilla Simple -> \'
+        System.out.println("comilla Doble: \""+ nombre +"\""); // Comilla Doble -> \"
         
-        System.out.println(i+j); //Se raliza la suma de numeros
-        System.out.println(i+j+usuario);//Evaliacion de izquierda a derecha
-        System.out.println(usuario+i+j);//Contecto cadena, todo es una cadena
-        System.out.println(usuario+(i+j));//uso de parentesis modifican la prioridad en la evaluacion
     }
 }
