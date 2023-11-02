363b6186c5ef7badd757942e6aa8efab2e2a542c
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-02 01:47:24 -0300

    Curso Java - Tipos Primitivos tipos flotantes  Cap.17

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -15,28 +15,17 @@ public class HolaMundo {
 
     public static void main(String args[]) {
         /*
-            tipos primitivos entero: byte (8 bits), short (16 bits), int (32 bits), long (64 bits)
+            tipos primitivos de tipo flotante: float (32 bits) y double (64 bits)
         */
-       byte numeroByte = (byte)129;
-        System.out.println("Valor byte: "+ numeroByte); 
-        System.out.println("Valor minimo byte: "+Byte.MIN_VALUE); 
-        System.out.println("Valor maximo byte: "+Byte.MAX_VALUE); 
-        
-        short numeroShort = (short)32768;
-        System.out.println("\nnumeroShort = " + numeroShort);
-        System.out.println("Valor minimo short: "+Short.MIN_VALUE); 
-        System.out.println("Valor maximo short: "+Short.MAX_VALUE); 
-        
-        int numeroInt = (int)2147483648L;
-        System.out.println("\nnumeroInt = " + numeroInt);
-        System.out.println("Valor minimo int: "+Integer.MIN_VALUE); 
-        System.out.println("Valor maximo int: "+Integer.MAX_VALUE); 
-        
-        long numeroLong = (long)9223372036854775808D;
-        System.out.println("\numeroLong = " + numeroLong);
-        System.out.println("Valor minimo long: "+Long.MIN_VALUE); 
-        System.out.println("Valor maximo long: "+Long.MAX_VALUE); 
-        
+        float numeroFloat = (float)3.4028235E38D;
+        System.out.println("numeroFloat = " + numeroFloat);
+        System.out.println("valor minimo float = " + Float.MIN_VALUE);
+        System.out.println("valor maximo float = " + Float.MAX_VALUE);
+     
+        double  numeroDouble = 1.7976931348623158E308D;
+        System.out.println("\nnumeroDouble = " + numeroDouble);
+        System.out.println("valor minimo double = " + Double.MIN_VALUE);
+        System.out.println("valor maximo double = " + Double.MAX_VALUE);
         
     }
 }
