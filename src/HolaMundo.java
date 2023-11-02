0355462dd786c7b9301eec8e435061899b85c2b8
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-02 02:00:06 -0300

    Curso Java - Inferencia con tipos var y primitivos  Cap.18

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -14,18 +14,13 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-        /*
-            tipos primitivos de tipo flotante: float (32 bits) y double (64 bits)
-        */
-        float numeroFloat = (float)3.4028235E38D;
-        System.out.println("numeroFloat = " + numeroFloat);
-        System.out.println("valor minimo float = " + Float.MIN_VALUE);
-        System.out.println("valor maximo float = " + Float.MAX_VALUE);
-     
-        double  numeroDouble = 1.7976931348623158E308D;
-        System.out.println("\nnumeroDouble = " + numeroDouble);
-        System.out.println("valor minimo double = " + Double.MIN_VALUE);
-        System.out.println("valor maximo double = " + Double.MAX_VALUE);
+        var numeroEntero = 10;
+        System.out.println("numeroEntero = " + numeroEntero);
         
+        var numeroDouble = 10.0;
+        System.out.println("numeroDouble = " + numeroDouble);
+        
+        var numeroFloat = 10.0F;
+        System.out.println("numeroFloat = " + numeroFloat);
     }
 }
