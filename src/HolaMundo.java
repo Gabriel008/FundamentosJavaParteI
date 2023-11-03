fdc6450d3367a72485d068a97cc37f8d7633d3b4
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-03 19:59:00 -0300

    Curso Java - Conversion de tipos primitivos en JAVA Cap.21-22

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -6,36 +6,35 @@ import java.util.Scanner;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author gabri
  */
 public class HolaMundo {
-
-    public static void main(String args[]) {
-        boolean varBoolean = true;
-        System.out.println("varBoolean = " + varBoolean);
-
-        if (varBoolean) {
-            System.out.println("La bandera es verdadera");
-        } else {
-            System.out.println("La bandera es falsa");
-        }
-            System.out.println("\n|---------------------------------|");
-            System.out.println("|Ingresa Edad:                    |");
-            System.out.println("|---------------------------------|");
-        Scanner consola = new Scanner(System.in);
-        var edad = consola;
     
-        //   var esAdulto = edad >= 18;
-        //    if (esAdulto) {
-            if (edad.nextInt() >= 18) {
-                System.out.println("\nEs mayor de edad");
-            } else {
-                System.out.println("\nEs menor de edad");
-            }
+    public static void main(String args[]) {
+        // Convertir tipo String a tipo int
 
+        var edad = Integer.parseInt("20");        //Suma el valor
+        //var edad = "20";        //Concatena el valor
+        System.out.println("edad = " + (edad + 1));
+        
+        var valorPI = Double.parseDouble("3.1416");
+        System.out.println("valorPI = " + valorPI);
 
+        //Pedir un valor
+        var consola =new Scanner(System.in);
+//        System.out.println("Proporciona tu edad: ");
+//        edad = Integer.parseInt(consola.nextLine());
+//        System.out.println("edad = " + edad);
+        var edadTexto = String.valueOf(10);
+        System.out.println("edadTexto = " + edadTexto);
+        
+        var caracter = "hola".charAt(0);
+        System.out.println("caracter = " + caracter);
+        
+        System.out.println("Proporciona un caracter: ");
+        caracter = consola.nextLine().charAt(0);
+        System.out.println("caracter = " + caracter);
     }
 }
