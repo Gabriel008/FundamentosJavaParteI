642e2f81910dd8b5bf2e5a8303e1e051b414b86b
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-04 16:36:33 -0300

    Curso Java - Precedencia de Operadorez Cap.31

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -11,12 +11,17 @@
 public class HolaMundo {
 
     public static void main(String args[]) {
-        var resultado = (1 > 2) ? "verdaddero" : "Falso"; // prueba logica ? Si es verdadero : Si es falso
+        var x = 5;
+        var y = 10;
+        var z = ++x + y--;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+
+        var resultado = 4 + 5 * 6 / 3;//-> 4 + ((5*6)/3)
         System.out.println("resultado = " + resultado);
 
-        var numero = 9;
-        resultado = (numero % 2 == 0) ? "Numero Par" : "Numero impar";
+        resultado = (4 + 5) * 6 / 3;
         System.out.println("resultado = " + resultado);
-
     }
 }
