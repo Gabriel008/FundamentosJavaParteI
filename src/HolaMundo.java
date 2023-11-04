ae30cceaf7aef5f622d51f243ad00f454099a2f8
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-04 11:59:20 -0300

    Curso Java - Operadores de igualdad y Relacionales Cap.27

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -12,45 +12,24 @@
  */
 public class HolaMundo {
     
-    public static void main(String args[]){
-     int a = 3;
-     var b = -a;
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+    public static void main(String args[]) {
+        var a = 3;
+        var b = 2;
         
-        var c = true;
-        var d = !c;
-        System.out.println("\nc = " + c);
+        var c = (a == b);
+        System.out.println("c = " + c);
+        
+        var d= a!=b;
+        
         System.out.println("d = " + d);
         
-        //incremento
-        //1. preincremento (simbolo antes de la variable)
-        var e =3;
-        var f =++e;//primero se incrementa la variable y despues se usa su valor
-        System.out.println("\ne = " + e);
+        var cadena="Hola";
+        var cadena2="Hola";
+        var e= cadena == cadena2;//compara referencias de objetos
+        System.out.println("e = " + e);
+        
+        var f = cadena.equals(cadena2);//compara contenido de cadenas
         System.out.println("f = " + f);
         
-           //2. postincremento (simbolo despues de la variable)
-           var g= 5;
-           var h = g++; //Primero se usa el valor y despues se incrementa
-           System.out.println("\ng = " + g);//teniamos pendiente un incremento
-           System.out.println("h = " + h);
-           
-        
-           //decremento
-           //1.predecremento
-           var i= 2;
-           var j = --i; //Primero se usa el valor y despues se incrementa
-           System.out.println("\ni = " + i);//ya esta decrementada
-           System.out.println("j = " + j);
-       
-           //1.prostdecremento
-           var k= 4;
-           var l= k--; //Primero se usa el valor y despues se incrementa
-           System.out.println("\nk = " + k);//tenia pediente un decremento
-           System.out.println("l = " + l);
-           
-           
-           
     }
 }
