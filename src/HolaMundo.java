8db4515881592727394c20319e64935694f3dcff
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-05 01:16:23 -0300

    Curso Java - Ejercicio ciclo WHILE  Cap.40

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -9,25 +9,11 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-        // var mes = 4;
-        System.out.println("Proporciona un valor entre 0 y 10: ");
-        var calificacion = "Valor desconocido";
-        var consola = new Scanner(System.in);
-        int valor = Integer.parseInt(consola.nextLine());
-
-        if (valor >= 9 && valor <= 10) {
-            calificacion = "A";
-        } else if (valor >= 8 && valor < 9) {
-            calificacion = "B";
-        } else if (valor >= 7 && valor < 8) {
-            calificacion = "C";
-        } else if (valor >= 6 && valor < 7) {
-            calificacion = "D";
-        } else if (valor >= 0 && valor < 6) {
-            calificacion = "F";
+        var contador = 0;
+        while (contador < 3) {
+            System.out.println("contador = " + contador);
+            contador++;
         }
-        System.out.println("\n|--------------------------------------------| ");
-        System.out.println("\tcalificacion = " + calificacion);
-        System.out.println("|--------------------------------------------| ");
+
     }
 }
