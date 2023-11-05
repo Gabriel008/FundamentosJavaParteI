e30875d10ebdb6428f193b8db31eae18aa434c15
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-05 18:23:32 -0300

    Curso Java - Objetos en Java-2 Cap.48

--- src/clases/PruebaPersona.java
+++ src/clases/PruebaPersona.java
@@ -3,12 +3,18 @@ package clases;
 public class PruebaPersona {
 
     public static void main(String[] args) { //psvm + tab
-        Persona persona1;
-        persona1 = new Persona();
+        Persona persona1 = new Persona();
 
         persona1.nombre = "Juan";
         persona1.apellido = "Perez";
         persona1.desplegarInformacion();
+
+        Persona persona2 = new Persona();
+        System.out.println("\npersona2 = " + persona2); // Valor espacio en memoria
+        persona2.desplegarInformacion();
+        persona2.nombre = "Karla";
+        persona2.apellido = "Lara";
+        persona2.desplegarInformacion();
 
     }
 }
