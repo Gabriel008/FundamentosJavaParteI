761b5067f1b569ae496ea4069dac372a3751a4e3
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-05 17:09:08 -0300

    Curso Java - Break y continue Cap.43

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -9,22 +9,28 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-        for (var contador = 0; contador < 3; contador++) {
+        for (var contador = 0; contador < 10; contador++) {
+            if (contador % 2 != 0) {
+                continue; // Ir a la siguiente iteracion
+            }
             System.out.println("contador = " + contador);
         }
 
+//        for (var contador = 0; contador < 10; contador++) {
+//            if (contador % 2 == 0) {
+//                System.out.println("contador = " + contador);
+//                break;
+//            }
+//        }
 //        var contador = 0;
 //        while (contador < 3) {
 //            System.out.println("contador = " + contador);
 //            contador++;
 //        }
-
-
 //        var contador = 0;
 //        do {
 //            System.out.println("contador = " + contador);
 //            contador++;
 //        } while (contador < 0); //Primero imprime y luego sale
-
     }
 }
