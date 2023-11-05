4684b604cbfd06fd9a25fd61e74ae49dbfbde621
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-04 21:04:27 -0300

    Curso Java - Sentencia de control if-else  Cap.34

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,6 +1,3 @@
-
-import java.util.Scanner;
-
 //Mi clase JAVA
 /**
  *
@@ -9,19 +6,13 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-        int numero1;
-        int numero2;
+        var condicion = false;
+        if (condicion) {
+            System.out.println("Condicion verdadera");
+            System.out.println("nueva linea");
+        } else {
+            System.out.println("Condicion falsa");
+        }
 
-        var consola = new Scanner(System.in);
-        System.out.println("Proporciona el numero1: ");
-        numero1 = Integer.parseInt(consola.nextLine());
-        System.out.println("Proporciona el numero2: ");
-        numero2 = Integer.parseInt(consola.nextLine());
-        
-        var mayor= numero1>=numero2 ? numero1:numero2;
-
-        System.out.println("\n1----------------------------------- 1");
-        System.out.println("\tEl numero mayor es: " +mayor);
-        System.out.println("1----------------------------------- 1");
     }
 }
