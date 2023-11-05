0630dec3f1b2afec7725b4a0b173e2bb1426c084
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-05 17:36:12 -0300

    Curso Java - Atributos y metodos Cap.46

--- src/clases/Persona.java
+++ src/clases/Persona.java
@@ -5,5 +5,14 @@ package clases;
  * @author gabri
  */
 public class Persona {
-    
+
+    //Atributos de la clase
+    String nombre;
+    String apellido;
+
+    //Metodos de la clase
+    public void desplegarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+    }
 }
