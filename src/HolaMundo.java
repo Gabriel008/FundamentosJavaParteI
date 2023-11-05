4c36e4c9dbe756f774d8964da7ae22ae0367b0aa
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-04 21:34:59 -0300

    Curso Java - ejercicio Calculo estcion del año con  if-else  Cap.36

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,5 +1,7 @@
-//Mi clase JAVA
 
+import java.util.Scanner;
+
+//Mi clase JAVA
 /**
  *
  * @author gabri
@@ -7,29 +9,22 @@
 public class HolaMundo {
 
     public static void main(String args[]) {
-        var condicion = false;
-        if (condicion) {
-            System.out.println("Condicion verdadera");
-            System.out.println("nueva linea");
-        } else {
-            System.out.println("Condicion falsa");
+        //var mes = 10;
+        var consola = new Scanner(System.in);
+        System.out.println("Inserte numero de mes (1 - 12): ");
+        int mes = Integer.parseInt(consola.nextLine());
+
+        var estacion = "Estacion Desconocida";
+        if (mes == 1 || mes == 2 || mes == 12) {
+            estacion = "Invierno";
+        } else if (mes == 3 || mes == 4 || mes == 5) {
+            estacion = "Primavera";
+        } else if (mes == 6 || mes == 7 || mes == 8) {
+            estacion = "Verano";
+        } else if (mes == 9 || mes == 10 || mes == 11) {
+            estacion = "Otonio";
         }
+        System.out.println("estacion = " + estacion);
 
-        var numero = 6;
-        var numeroTexto = "Numero desconocido";
-
-        if (numero == 1) {
-            numeroTexto = "Numero uno";
-        } else if (numero == 2) {
-            numeroTexto = "Numero dos";
-        } else if (numero == 3) {
-            numeroTexto = "Numero tres";
-        } else if (numero == 4) {
-            numeroTexto = "Numero cuatro";
-        } else {
-            numeroTexto = "Numero no encontrado";
-        }
-
-        System.out.println("numeroTexto = " + numeroTexto);
     }
 }
