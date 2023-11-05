03b86dc1e926b6fbb28a761254dbb8683dc5f9b8
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-05 00:55:31 -0300

    Curso Java - Ejercicio Sistema de Calificaciones  Cap.39

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -10,33 +10,24 @@ public class HolaMundo {
 
     public static void main(String args[]) {
         // var mes = 4;
-        System.out.println("Ingrese numero de mes: ");
-        var estacion = "Estacion Desconocida";
+        System.out.println("Proporciona un valor entre 0 y 10: ");
+        var calificacion = "Valor desconocido";
         var consola = new Scanner(System.in);
-        int mes = Integer.parseInt(consola.nextLine());
+        int valor = Integer.parseInt(consola.nextLine());
 
-        switch (mes) {
-            case 1:
-            case 2:
-            case 12:
-                estacion = "Invierno";
-                break;
-            case 3:
-            case 4:
-            case 5:
-                estacion = "Primavera";
-                break;
-            case 6:
-            case 7:
-            case 8:
-                estacion = "Verano";
-                break;
-            case 9:
-            case 10:
-            case 11:
-                estacion = "Otonio";
-                break;
+        if (valor >= 9 && valor <= 10) {
+            calificacion = "A";
+        } else if (valor >= 8 && valor < 9) {
+            calificacion = "B";
+        } else if (valor >= 7 && valor < 8) {
+            calificacion = "C";
+        } else if (valor >= 6 && valor < 7) {
+            calificacion = "D";
+        } else if (valor >= 0 && valor < 6) {
+            calificacion = "F";
         }
-        System.out.println("Estacion = " + estacion);
+        System.out.println("\n|--------------------------------------------| ");
+        System.out.println("\tcalificacion = " + calificacion);
+        System.out.println("|--------------------------------------------| ");
     }
 }
